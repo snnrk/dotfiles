6cd4084ba1b32180404d4587d7999ccc186c3b01
OSANAI Noriaki <snnrk1002@gmail.com>
2024-03-06 10:55:35 +0900

    fix: update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,14 @@
 
 ROOTDIR=$(cd $(dirname $0); pwd)
 
+if [ -f $HOME/.zshrc ]
+then
+  mv $HOME/.zshrc{,.orig}
+fi
 ln -s $ROOTDIR/zsh/.zshrc $HOME/.zshrc
+
+if [ -d $HOME/.zsh.conf.d ]
+then
+  mv $HOME/.zsh.conf.d{,.orig}
+fi
 ln -s $ROOTDIR/zsh/.zsh.conf.d $HOME/.zsh.conf.d
